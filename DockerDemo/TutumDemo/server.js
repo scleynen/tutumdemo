b55ff6d8bf896b51a32462c8ad2af2ff7d6a1c9b
scleynen <steve.cleynen@hotmail.com>
2016-02-23 10:45:59 +0100

    Fixed port issue

--- DockerDemo/TutumDemo/server.js
+++ DockerDemo/TutumDemo/server.js
@@ -1,6 +1,8 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('process: '  + process.pid.toString());
-}).listen(port);
+    res.end('process: ' + process.pid.toString());
+}).listen(port, function () {
+    console.log('server is listening on port %d', port);
+});
