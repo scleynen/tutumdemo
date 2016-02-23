7bd4a46cb2ea62eb689e380f9db29320544dd805
scleynen <steve.cleynen@hotmail.com>
2016-02-23 15:35:08 +0100

    test

--- DockerDemo/TutumDemo/server.js
+++ DockerDemo/TutumDemo/server.js
@@ -1,8 +1,8 @@
 ﻿var http = require('http');
-var port = process.env.port || 8080;
+var port = 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('process: ' + process.pid.toString());
+    res.end('Test Process: ' + process.pid.toString());
 }).listen(port, function () {
     console.log('server is listening on port %d', port);
 });
